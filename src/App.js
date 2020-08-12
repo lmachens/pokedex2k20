2f72ba5bfc53120f033b6e47e49fa0cae852717f
lmachens <leon.machens@googlemail.com>
2020-08-12 13:36:28 +0200

    Add List component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import List from "./components/List";
 
 function App() {
   return (
@@ -7,7 +8,20 @@ function App() {
       <header>
         Pokedex <input />
       </header>
-      <main className="colorful-border">Pokemons</main>
+      <main className="colorful-border">
+        <List>
+          Dies ist ein Children von List
+          {/* <ListItem href="#">
+            <ListItemIcon src="" />
+            <ListItemText primary="Bulbasaur" secondary="#001" />
+          </ListItem>
+
+          <ListItem href="#">
+            <ListItemIcon src="" />
+            <ListItemText primary="Ivysaur" secondary="#002" />
+          </ListItem> */}
+        </List>
+      </main>
       <footer>Footer</footer>
     </div>
   );
---
+++ src/components/List.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function List(props) {
+  return <nav>{props.children}</nav>;
+}
+
+export default List;
